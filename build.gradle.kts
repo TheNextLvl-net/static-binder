d72d74a840a85808ab5ed1a4d36fee9fcad32ae2
david <mail@nonswag.dev>
2025-11-01 20:22:47 +0100

    Add Gradle wrapper scripts and configs

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("java")
+}
+
+group = "net.thenextlvl"
+version = "0.1.0"
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+}
+
+tasks.compileJava {
+    options.release.set(21)
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("org.jetbrains:annotations:26.0.2-1")
+    compileOnly("org.jspecify:jspecify:1.0.0")
+    
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
