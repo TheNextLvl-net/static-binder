0a7e65843598367e2a2dcf10c1ed5654bbfbc3e6
david <mail@nonswag.dev>
2025-11-01 20:31:51 +0100

    Enable Maven publishing with repo config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("maven-publish")
 }
 
 group = "net.thenextlvl"
@@ -20,7 +21,7 @@ repositories {
 dependencies {
     compileOnly("org.jetbrains:annotations:26.0.2-1")
     compileOnly("org.jspecify:jspecify:1.0.0")
-    
+
     testImplementation(platform("org.junit:junit-bom:6.0.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -28,4 +29,26 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        artifactId = "static-binder"
+        groupId = "net.thenextlvl"
+        pom.scm {
+            val repository = "TheNextLvl-net/static-binder"
+            url.set("https://github.com/$repository")
+            connection.set("scm:git:git://github.com/$repository.git")
+            developerConnection.set("scm:git:ssh://github.com/$repository.git")
+        }
+        from(components["java"])
+    }
+    repositories.maven {
+        val branch = if (version.toString().contains("-pre")) "snapshots" else "releases"
+        url = uri("https://repo.thenextlvl.net/$branch")
+        credentials {
+            username = System.getenv("REPOSITORY_USER")
+            password = System.getenv("REPOSITORY_TOKEN")
+        }
+    }
 }
